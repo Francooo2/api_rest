0a0d53c1beb9c5949193d81de15ee3a99c674872
Franco Palma <fpalmalopez2@gmail.com>
2022-03-08 20:45:09 -0300

    Ajuste en controllers

--- controllers/products.js
+++ controllers/products.js
@@ -15,29 +15,17 @@ const getProducts = async (req, res) => {
 }
 
 const getProduct = async (req, res) => {
-    
-    const name = req.params.name
-    
-    if( name[name.length-1] === '°' ) {
-        const sql = 'SELECT * FROM product WHERE SUBSTRING(RTRIM(UPPER(name)), 0, LENGTH(RTRIM(UPPER(name))) - 1) = ?'
-        await pool.query(sql, name.substring(0, name.length - 1), (error, results) => {
-            if (error) {
-                res.status(404).send('Servicio no disponible.')
-            } else {
-                res.status(200).json(results)
-            }
-        })
-    } else {
-        const sql = 'SELECT * FROM product WHERE RTRIM(UPPER(name)) = ?'
-        await pool.query(sql, name, (error, results) => {
-            if (error) {
-                res.status(404).send('Servicio no disponible.')
-            } else {
-                res.status(200).json(results)
-            }
-        })
-    }
 
+    const name = req.params.name
+    const sql = 'SELECT * FROM product WHERE RTRIM(UPPER(name)) = ?'
+
+    await pool.query(sql, name, (error, results) => {
+        if (error) {
+            res.status(404).send('Servicio no disponible.')
+        } else {
+            res.status(200).json(results)
+        }
+    })
 
 }
 
